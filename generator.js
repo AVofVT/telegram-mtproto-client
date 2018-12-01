6253f77ac86bd51fad8589a50a654b2898e2a24e
Jake <jake.arefyev@gmail.com>
2018-12-01 18:38:17 +0200

    see link to core.telegram.org

--- generator.js
+++ generator.js
@@ -44,7 +44,7 @@ const typeTpl = (ctor) => {
     return `
 /**
  *
- * @see {@link https://github.com/wfjsw/telegram-core-docs/blob/master/constructor/${ctor.name}.md}
+ * @see {@link https://core.telegram.org/constructor/${ctor.name}.md}
  * @typedef {${ctor.type}} ${ctor.name}` + ctor.args.map(p =>
     `\n * @property {${p.type}} ${p.name}` ).join('') + `\n */`
 }
@@ -53,7 +53,7 @@ const descriptionTpl = (method) => {
     const hasParams = method.args && method.args.length
     return `
     /**
-     * @see {@link https://github.com/wfjsw/telegram-core-docs/blob/master/method/${method.name}.md}` +
+     * @see {@link https://core.telegram.org/method/${method.name}.md}` +
      `${hasParams ? '\n     * @param {object} config': ''}` +
      method.args.map(p =>  `\n     * @property {${p.type}} config.${p.name}` ).join('') + `
      * @returns {${method.return}}
